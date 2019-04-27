632c64abd268177190f9a20e1f1fc272052f3f00
Administrator <djava7@126.com>
2019-04-27 21:01:44 +0800

    test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -18,7 +18,7 @@ public class Application {
 
     private RestTemplate restTemplate = new RestTemplate();
 
-    private String url = "http://localhost:9001";
+    private String url = "http://order:9001";
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
     @RequestMapping("/")
@@ -33,26 +33,7 @@ public class Application {
             System.out.println("rtn:" + response.getBody());
         }
 
-        return response.getBody();
-    }
-
-    @RequestMapping("/memext")
-    public String memExt(@RequestParam(required = false, defaultValue = "999999") Integer max) {
-        if (max == null) {
-            max = 999999;
-        } else if (max == 0) {
-            MapTest.clearMem();
-        }
-        MapTest.test1(max);
-        return "mem auto extend test.";
-    }
-
-    @RequestMapping("/memget")
-    public String memGet() {
-        long i = Runtime.getRuntime().totalMemory() / 1024 / 1024;// Java 虚拟机中的内存总量，以字节为单位
-        long j = Runtime.getRuntime().freeMemory() / 1024 / 1024;// Java 虚拟机中的空闲内存量
-        long k = i - j;// Runtime.getRuntime().maxMemory() / 1024 / 1024;
-        return "总的内存量为:" + i + "Mb" + ";空闲内存量:" + j + "Mb" + ";在用内存量:" + k + "Mb";
+        return "customer-order:" + response.getBody();
     }
 
     public static void main(String[] args) {
